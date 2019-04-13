2e87f3abdd9348e209700fe57a8f5b9cacb01a0c
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2019-04-13 18:08:40 +0200

    Calculator ready

--- dev/mainJs.js
+++ dev/mainJs.js
@@ -1,4 +1,48 @@
 import css from "./mainCss.css";
 
+const submitButton = document.querySelector(`.calculator__submitButton`),
+    billSum = document.querySelector(`.calculator__input`),
+    tipSelect = document.querySelector(`.calculator__tipSelect`);
 
-console.log(`WItam w kalku !!`);
+
+/**
+ *  Checks if given string contains only numbers
+ * @param {string} value
+ */
+const checkIfNumber = value => Number.isFinite(Number(value)) ? false : true;
+
+
+/**
+ *  Checks if given string contains any character
+ * @param {string} value
+ */
+const checkIfEmptyValue = (value) => value.length < 1 ? true : false;
+
+
+/**
+ * Calculating tip value
+ * @return {number} tip value
+ */
+const calculateTip = () => {
+
+    let bill = billSum.value,
+        tipPercent = tipSelect.value;
+
+    if (checkIfEmptyValue(bill)) {
+        console.log(`Podaj wartość rachunku`);
+    } else if (checkIfNumber(bill)) {
+        console.log(`Podany rachunek musi być liczbą`);
+    } else {
+
+        return bill * tipPercent;
+
+    }
+
+    return 0;
+}
+
+
+//  Events
+
+submitButton.addEventListener(`click`, () => document.querySelector(`.calculator__result`).innerHTML = `Napiwek wynosi: ${calculateTip()} `);
+
